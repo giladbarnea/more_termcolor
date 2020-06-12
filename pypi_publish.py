7079c8ff61a605948dee9a99fd6c179d0f64db3a
Gilad Barnea <giladbrn@gmail.com>
2020-06-12 11:46:46 +0300

    pypi_publish.py

--- pypi_publish.py
+++ pypi_publish.py
@@ -1,4 +1,3 @@
-# env/bin/python3
 from more_termcolor import util
 import re
 import semver
@@ -18,16 +17,16 @@ def main():
     with open('./setup.py') as f:
         data = f.read()
     VERSION_RE = re.compile(r"\s*version='(?P<ver>\d+(?:\.\d+)+)',")
-    version = VERSION_RE.fullmatch(data).groupdict()['ver']
+    version = VERSION_RE.search(data).groupdict()['ver']
     parsed = semver.VersionInfo.parse(version)
     bumped = parsed.bump_patch()
     if bumped.patch == 10:
         bumped = parsed.bump_minor()
     if util.confirm(f'current version is {version}, bump to {bumped}?'):
-        replaced = VERSION_RE.sub(str(bumped), version)
+        replaced = data.replace(version, str(bumped), 1)
         if is_dry_run():
-            diff = set(data).difference(set(replaced))
-            print('dry run: would have made this changes to setup.py:\n', diff)
+            before, after = map(str.strip, set(data.splitlines()).symmetric_difference(set(replaced.splitlines())))
+            print('dry run: would have made the following changes to setup.py;', 'before:', before, 'after:', after, sep='\n')
         
         else:
             with open('./setup.py', 'w'):
